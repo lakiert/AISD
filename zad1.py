8ae85ef5c766d4863d3eb4b86476d37d1eda6f1b
lakiert <73653530+lakiert@users.noreply.github.com>
2021-10-07 18:07:11 +0200

    zad1

---
+++ zad1.py
@@ -0,0 +1,92 @@
+# zad1
+
+# x='J'
+# y='Kowalski'
+#
+# def funkcja(x,y):
+#     return x + '.' + y
+#
+# print (funkcja(x,y))
+# print (funkcja('A' , 'Nowak'))
+
+
+# zad2
+
+# imie = 'andrzej'
+# nazwisko = 'kowalski'
+#
+# def funkcja(x,y):
+#     return x[0].capitalize() + '.' + y.capitalize()
+#
+# print(funkcja(imie,nazwisko))
+# print(funkcja('janina','nowak'))
+
+
+# zad3
+
+# pierwsze=20
+# drugie=21
+# wiek=21
+#
+# def funkcja(x,y,z):
+#     rok = (x*100)+y
+#     wynik = rok - z
+#     return wynik
+#
+# print(funkcja(pierwsze,drugie,wiek))
+# print(funkcja(20,21,45))
+
+
+# zad4
+
+# imie = 'andrzej'
+# nazwisko = 'kowalski'
+#
+# def funkcja1(x,y):
+#     return x[0].capitalize() + '.' + y.capitalize()
+#
+# def funkcja2(x,y,foo):
+#     return foo(x,y)
+#
+# print(funkcja2(imie,nazwisko,funkcja1))
+# print(funkcja2('jan','nowak',funkcja1))
+
+
+# zad5
+
+# def funkcja(x,y):
+#     if x>0 & (y>0 & y!=0):
+#         return x/y
+#
+# print(funkcja(10,5))
+# print(funkcja(-20,10))
+# print(funkcja(10,0))
+
+
+# zad6
+
+# wynik=0
+# limit=100
+#
+# while wynik < limit:
+#     i= int(input("podaj liczbe:"))
+#     wynik += i
+#
+# print("wynik=", wynik)
+
+
+# zad7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
