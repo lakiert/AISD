565a8819209068acb380338dbce3fec8263e4e4b
lakiert <73653530+lakiert@users.noreply.github.com>
2021-11-18 17:32:46 +0100

    Add files via upload

---
+++ zad3.py
@@ -0,0 +1,57 @@
+# zad1
+
+# def numbers(n: int):
+#
+#     if (n < 0):
+#         return
+#
+#     print(n)
+#     numbers(n-1)
+#
+#
+# numbers(10)
+
+
+
+# zad2
+
+# def fib(n: int):
+#
+#     if n==0:
+#         return 0
+#     if n==1:
+#         return 1
+#
+#     else:
+#         return fib(n-1) + fib(n-2)
+#
+#
+# print(fib(0))
+# print(fib(1))
+# print(fib(2))
+# print(fib(3))
+# print(fib(4))
+# print(fib(5))
+# print(fib(6))
+# print(fib(7))
+# print(fib(8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
