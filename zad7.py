ef7224019ab74a89c6a72d33d46c2ecc2cc42050
lakiert <73653530+lakiert@users.noreply.github.com>
2021-12-23 17:17:04 +0100

    Update zad7.py

--- zad7.py
+++ zad7.py
@@ -9,10 +9,31 @@ class Vertex:
     data: Any
     index: int
 
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
 class Edge:
     source: Vertex
     destination: Vertex
     weight: Optional[float]
 
+    def __init__(self, source, destination, weight):
+        self.source = source
+        self.destination = destination
+        self.weight = weight
+
+
 class Graph:
     adjacencies: Dict[Vertex, List[Edge]]
+
+    def __init__(self):
+        self.adjacencies = {}
+
+    def create_vertex(self, value):
+        vertex = Vertex(value, len(self.adjacencies))
+        self.adjacencies[vertex] = []
+
+    def add_directed_edge(self, source, destination, weight):
+        edge = Edge(source, destination, weight)
+        self.adjacencies[source].append(edge)
