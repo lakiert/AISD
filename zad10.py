ad9075e72179a829f3481a4002e2b78f8bd3a9a4
lakiert <73653530+lakiert@users.noreply.github.com>
2022-01-20 17:37:46 +0100

    Create zad10.py

---
+++ zad10.py
@@ -0,0 +1,24 @@
+from array import array
+from ctypes import Array
+from typing import Tuple
+
+
+def binary_search(numbers: Array, value: int) -> Tuple[bool, int]:
+    start, end = 0, len(numbers) - 1
+
+    while start <= end:
+        middle = (start + end) // 2
+        if numbers[middle] == value:
+            return True, middle
+        elif numbers[middle] < value:
+            start += 1
+        elif numbers[middle] > value:
+            end -= 1
+
+    return False, -1
+
+
+ints = array('I', [1, 5, 6, 7, 10, 26, 29, 40])
+
+result = binary_search(ints, 40)
+print(result)
